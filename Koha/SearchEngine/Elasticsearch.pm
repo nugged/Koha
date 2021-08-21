429f80526d9740b839409f8939b1150a769bfa90
Andrew Nugged <nugged@gmail.com>
2021-08-21 11:40:58 +0300

    Bug 28736: (QA follow-up) hints in explanation about broken syntax
    Add direct hint for the user what might be the issues
    with search phrase.
    
    In most cases we have unescaped or not removed special characters
    when user copy-pastes book name. If we explain that error happens
    because there is special search language, user will know what to
    research because we explicitly will tell that this is NOT the
    empty result, but search wasn't performed.

--- Koha/SearchEngine/Elasticsearch.pm
+++ Koha/SearchEngine/Elasticsearch.pm
@@ -1530,7 +1530,9 @@ sub process_error {
     warn $msg;    # simple logging
 
     # This is super-primitive
-    return __("Unable to understand your search query, please rephrase and try again.\n")
+    return __("There might be results, but there is ElasticSearch query language syntax error in your search request: remove or ".
+           "escape with backslash ES-language reserved characters (those are: + - = && || > < ! ( ) { } [ ] ^ \" ~ * ? : \ / ) ".
+           "or change it to be the proper ES-language request and try again.\n")
         if $msg =~ /ParseException|parse_exception/;
 
     return __("Unable to perform your search. Please try again.\n");
