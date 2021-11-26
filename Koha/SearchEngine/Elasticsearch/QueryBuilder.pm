9e8912d9f575fe3f3a4f02a289f117343b8e808e
Andrew Nugged <nugged@gmail.com>
2021-11-26 13:45:37 +0200

    Bug 00000: HOTFIX. Warn removal: s/// when undef allowed
    Use of uninitialized value $field in substitution (s///) at .../Koha/SearchEngine/Elasticsearch/QueryBuilder.pm line 1099.

--- Koha/SearchEngine/Elasticsearch/QueryBuilder.pm
+++ Koha/SearchEngine/Elasticsearch/QueryBuilder.pm
@@ -1268,7 +1268,8 @@ sub _fix_limit_special_cases {
             push @new_lim, "date-of-acquisition.raw:$date";
         } else {
             my ( $field, $term ) = $l =~ /^\s*([\w,-]*?):(.*)/;
-            $field =~ s/,phr$//;    #We are quoting all the limits as phrase, this prevents from quoting again later
+            $field =~ s/,phr$//    #We are quoting all the limits as phrase, this prevents from quoting again later
+                if defined $field;
             if ( defined($field) && defined($term) ) {
                 push @new_lim, "$field:(\"$term\")";
             } else {
