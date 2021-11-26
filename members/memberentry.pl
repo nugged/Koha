ad816cab220e50b13e1e3952eaacbc83bf76933c
Andrew Nugged <nugged@gmail.com>
2021-11-26 13:45:37 +0200

    Bug 00000: HOTFIX. Warn removal: cardnumber already can be undef
    Use of uninitialized value $newdata{"cardnumber"} in pattern match (m//) at .../members/memberentry.pl line 326.

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -363,7 +363,7 @@ my $extended_patron_attributes;
 if ( $op eq 'cud-save' || $op eq 'cud-insert' ) {
 
     # If the cardnumber is blank, treat it as null.
-    $newdata{'cardnumber'} = undef if $newdata{'cardnumber'} =~ /^\s*$/;
+    $newdata{'cardnumber'} = undef if defined $newdata{'cardnumber'} and $newdata{'cardnumber'} =~ /^\s*$/;
 
     my $new_barcode = $newdata{'cardnumber'};
     Koha::Plugins->call( 'patron_barcode_transform', \$new_barcode );
