fbb11da91288d15f05319cb28b057329eccac2b4
Andrew Nugged <nugged@gmail.com>
2021-11-26 13:45:37 +0200

    Bug 00000: HOTFIX. Warn removal: discount treated everywhere as number, To check: yet in DB it's float with NULLs allowed, btw... For a reason?
    TO RESEARCH:
        one customer has 50% orders with discount NULL
        and 50% with 0.0 - check why, and constrain the state.
    
        aqbooksellers table also have discount field which can be NULL,
        and also there 15% with 0.0 and 85% records with NULL
    
    Argument "" isn't numeric in numeric gt (>) at .../C4/Acquisition.pm line 2855.
    Use of uninitialized value in sprintf at .../acqui/neworderempty.pl line 478.

--- acqui/neworderempty.pl
+++ acqui/neworderempty.pl
@@ -463,7 +463,7 @@ $template->param(
     suggestion         => $suggestion,
     biblionumber       => $biblionumber,
     uncertainprice     => $data->{'uncertainprice'},
-    discount_2dp       => sprintf( "%.2f", $bookseller->discount ),      # for display
+    discount_2dp       => sprintf( "%.2f", $bookseller->discount || 0 ), # for display
     discount           => $bookseller->discount,
     orderdiscount_2dp  => sprintf( "%.2f", $data->{'discount'} || 0 ),
     orderdiscount      => $data->{'discount'},
