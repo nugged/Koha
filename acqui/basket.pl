06c094fe008319addd1b83617263b6b8b33a435a
Andrew Nugged <nugged@gmail.com>
2021-11-26 13:45:37 +0200

    Bug 00000: NOTES. UX: this die should be explained for the user

--- acqui/basket.pl
+++ acqui/basket.pl
@@ -708,6 +708,8 @@ sub edi_close_and_order {
 
             #$template->param( edifile => 1 );
         }
+        # FIXME: uncaught exception 'Koha::Exceptions::Acquisition::Basket::AlreadyClosed'
+        # makes site 500 error and logs only  'Basket is already closed'
         Koha::Acquisition::Baskets->find($basketno)->close;
 
         # if requested, create basket group, close it and attach the basket
