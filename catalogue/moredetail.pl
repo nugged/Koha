2302349db0d718e2e78b9b2e091f17cd48cbb402
Andrew Nugged <nugged@gmail.com>
2022-07-16 20:13:52 +0300

    Bug 00000: NOTES. DIEs
    These are 500, dies, not warnings!

--- catalogue/moredetail.pl
+++ catalogue/moredetail.pl
@@ -55,6 +55,8 @@ my $biblionumber;
 my $itemnumber;
 if ( $query->param('itemnumber') && !$query->param('biblionumber') ) {
     $itemnumber = $query->param('itemnumber');
+    # TODO: prevent DIEs,
+    # Can't call method "biblionumber" on an undefined value at /usr/share/koha/intranet/cgi-bin/catalogue/moredetail.pl line 79.
     my $item = Koha::Items->find($itemnumber);
     $biblionumber = $item->biblionumber;
 } else {
