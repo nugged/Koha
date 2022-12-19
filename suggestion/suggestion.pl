d9d87206848fc41e0e5f9643b4af1f033da39ea1
Andrew Nugged <nugged@gmail.com>
2022-12-19 14:39:00 +0200

    Bug 00000: HOTFIX. Warn removal uninitialized value in string eq
    Use of uninitialized value in string eq at /usr/share/koha/intranet/cgi-bin/suggestion/suggestion.pl line 115.
    
    Use of uninitialized value $v in string eq at /usr/share/koha/intranet/cgi-bin/suggestion/suggestion.pl line 117.
    
    Use of uninitialized value in string eq at /usr/share/koha/intranet/cgi-bin/suggestion/suggestion.pl line 417.

--- suggestion/suggestion.pl
+++ suggestion/suggestion.pl
@@ -125,12 +125,12 @@ foreach my $key ( keys %$suggestion_ref ) {
     delete $suggestion_ref->{$key} if $key =~ m{^DataTables_acqui_suggestions_suggestions};
 }
 
-delete $suggestion_only->{branchcode} if $suggestion_only->{branchcode} eq '__ANY__';
-delete $suggestion_only->{budgetid}   if $suggestion_only->{budgetid} eq '__ANY__';
+delete $suggestion_only->{branchcode} if defined $suggestion_only->{branchcode} and $suggestion_only->{branchcode} eq '__ANY__';
+delete $suggestion_only->{budgetid}   if defined $suggestion_only->{budgetid}   and $suggestion_only->{budgetid} eq '__ANY__';
 
 unless ( $op eq 'cud-save' ) {
     while ( my ( $k, $v ) = each %$suggestion_only ) {
-        delete $suggestion_only->{$k} if $v eq '';
+        delete $suggestion_only->{$k} if ! defined $v or $v eq '';
     }
 }
 
@@ -452,7 +452,8 @@ if ( $op eq 'else' ) {
     }
     for my $f (qw (branchcode budgetid)) {
         delete $search_params->{$f}
-            if $search_params->{$f} eq '__ANY__'
+            if ! defined $search_params->{$f}
+            || $search_params->{$f} eq '__ANY__'
             || $search_params->{$f} eq '';
     }
     for my $bi (qw (title author isbn publishercode copyrightdate collectiontitle)) {
