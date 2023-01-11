09f107ab8f24b89ebac8187971540bdc83c07fbc
Andrew Nugged <nugged@gmail.com>
2023-01-11 19:44:29 +0200

    Bug 00000: Fix componentparts "show all" link httpencoded wrongly

--- opac/opac-detail.pl
+++ opac/opac-detail.pl
@@ -636,9 +636,9 @@ if ( $showcomp eq 'both' || $showcomp eq 'opac' ) {
                 );
         }
         $template->param( ComponentParts => $parts );
-        my ( $comp_query, $comp_query_str, $comp_sort ) = $biblio->get_components_query;
-        my $cpq = $comp_query_str . "&sort_by=" . $comp_sort;
-        $template->param( ComponentPartsQuery => $cpq );
+        my ( undef, $comp_query_str, $comp_sort ) = $biblio->get_components_query;
+        $template->param( ComponentPartsQuery => $comp_query_str );
+        $template->param( ComponentPartsQuerySort => $comp_sort );
     }
 } else {    # check if we should show analytics anyway
     $show_analytics = 1 if @{ $biblio->get_marc_components(1) };    # count matters here, results does not
