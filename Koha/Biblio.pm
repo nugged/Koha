42a4d9ef8952f55fa9d650bdace9e92270daa675
Andrew Nugged <nugged@gmail.com>
2023-02-23 04:06:44 +0200

    Bug 00000: make componentpars also work for bib-levels m and s

--- Koha/Biblio.pm
+++ Koha/Biblio.pm
@@ -957,7 +957,7 @@ sub get_components_query {
             }
 
             # limit to monograph and serial component part records
-            $searchstr .= " AND (bib-level:a OR bib-level:b)";
+            $searchstr .= " AND (bib-level:a OR bib-level:b OR bib-level:m OR bib-level:s)";
             $searchstr .= ")";
         }
     } else {
