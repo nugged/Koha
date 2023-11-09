20e8780d35853f7c2768d7d04f4ce061439e16bf
Petro Vashchuk <stalkernoid@gmail.com>
2023-11-09 18:17:18 +0200

    Bug 00000: Add EstimateDeliveryByOpenDate feature and syspref

--- installer/data/mysql/mandatory/sysprefs.sql
+++ installer/data/mysql/mandatory/sysprefs.sql
@@ -271,6 +271,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('ERMProviderEbscoApiKey', '', NULL, 'API key for EBSCO', 'Free'),
 ('ERMProviderEbscoCustomerID', '', NULL, 'Customer ID for EBSCO', 'Free'),
 ('ERMProviders', 'local', 'local|ebsco', 'Set the providers for the ERM module', 'Choice'),
+('EstimateDeliveryByBasketDate', '0', NULL, 'Calculate the estimated delivery date from the time the basket is opened, instead of when it is closed.', 'YesNo'),
 ('ExcludeHolidaysFromMaxPickUpDelay', '0', NULL, 'If ON, reserves max pickup delay takes into accountthe closed days.', 'YesNo'),
 ('expandedSearchOption','0',NULL,'If ON, set advanced search to be expanded by default','YesNo'),
 ('ExpireReservesAutoFill','0',NULL,'Automatically fill the next hold with a automatically canceled expired waiting hold.','YesNo'),
