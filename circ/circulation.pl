9ca1c7460da387f63183926d035e8c2de75db22f
Andrii Nugged <nugged@gmail.com>
2024-02-13 06:04:41 +0200

    Bug 00000: DEBUG. Deeper analyze dies 2.2
    Can't call method "param" on an undefined value at /usr/share/koha/intranet/cgi-bin/circ/circulation.pl line 164.
      POST: /intranet/circ/circulation.pl
     Form parameters: userid, branch, koha_login_context, findborrower, password

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -182,8 +182,8 @@ if ( ! defined $session ) {
     warn "Undefined session, but it's not expected to Koha reach here! \$sessionID=".($sessionID//'-undef-')."\n";
 }
 
-my $stickyduedate      = $query->param('stickyduedate')      || $session->param('stickyduedate');
-my $duedatespec        = $query->param('duedatespec')        || $session->param('stickyduedate');
+my $stickyduedate      = $query->param('stickyduedate')      || $session && $session->param('stickyduedate');
+my $duedatespec        = $query->param('duedatespec')        || $session && $session->param('stickyduedate');
 my $restoreduedatespec = $query->param('restoreduedatespec') || $duedatespec || $session->param('stickyduedate');
 if ( $restoreduedatespec && $restoreduedatespec eq "highholds_empty" ) {
     undef $restoreduedatespec;
