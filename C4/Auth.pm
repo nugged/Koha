7346707be3f157dceed7785961373c651ee4b6dd
Andrii Nugged <nugged@gmail.com>
2024-02-13 06:00:10 +0200

    Bug 00000: DEBUG. Deeper analyze dies 1
     Can't use an undefined value as a HASH reference at /usr/share/koha/lib/C4/Auth.pm line 985.
      GET: /cgi-bin/koha/mainpage.pl?logout.x=1

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -999,6 +999,10 @@ sub checkauth {
 
     if ($logout) {
 
+        if ( ! defined C4::Context->userenv ) {
+            warn "Undefined userenv but expected to be defined. \$sessionID=".($sessionID//'-undef-')."\n";
+        }
+
         # voluntary logout the user
         # check whether the user was using their shibboleth session or a local one
         my $shibSuccess = C4::Context->userenv ? C4::Context->userenv->{'shibboleth'} : undef;
