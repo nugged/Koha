cffb9b3a0b6db0eeb95d603b1be19244bc9bd1a8
Andrii Nugged <nugged@gmail.com>
2024-11-26 22:06:27 +0200

    Bug 00000: HOTFIX. Warn removal: GetIssuingCharges returns undef (shoultd it, btw?)
    [WARN] Use of uninitialized value $charge in numeric gt (>) at .../C4/Circulation.pm line 3037.

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -3537,7 +3537,7 @@ sub AddRenewal {
 
             # Charge a new rental fee, if applicable
             my ( $charge, $type ) = GetIssuingCharges( $itemnumber, $borrowernumber );
-            if ( $charge > 0 ) {
+            if ( $charge && $charge > 0 ) {
                 AddIssuingCharge( $issue, $charge, 'RENT_RENEW' );
             }
 
