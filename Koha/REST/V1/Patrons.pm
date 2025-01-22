55732c1a56d02ebee191ed7a45d036a1e159381c
Andrii Nugged <nugged@gmail.com>
2025-01-23 00:43:48 +0200

    Bug 00000: Make API at all not fail with readOnly params

--- Koha/REST/V1/Patrons.pm
+++ Koha/REST/V1/Patrons.pm
@@ -28,6 +28,8 @@ use List::MoreUtils qw(any);
 use Scalar::Util    qw( blessed );
 use Try::Tiny       qw( catch try );
 
+use JSON qw();
+
 =head1 NAME
 
 Koha::REST::V1::Patrons
@@ -246,12 +248,17 @@ Controller function that handles updating a Koha::Patron object
 
 sub update {
     my $c = shift;
-    if($c->tx->req->headers->user_agent =~ /VuFind/) {
-        my $body = $c->req->json;
-        delete $body->{expired} if exists $body->{expired};
-        $c->req->body(JSON::encode_json($body));
-        warn "[HOTFIX!] VuFind detected, removing expired field from request body";
+
+    # Forcibly Remove readOnly fields from the request body:
+    my $body = $c->req->json || {};
+    for my $field (qw(expired restricted anonymized)) {
+        if (exists $body->{$field}) {
+            delete $body->{$field};
+            # warn "[HOTFIX!] Removing read-only field '$field' from request body";
+        }
     }
+    $c->req->body(JSON::encode_json($body));
+
     $c = $c->openapi->valid_input or return;
 
     my $patron = Koha::Patrons->find( $c->param('patron_id') );
