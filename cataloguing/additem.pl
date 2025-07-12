bb6461695beb5517abbe68313a7019d1986589a7
Andrii Nugged <nugged@gmail.com>
2025-07-12 21:53:00 +0300

    Bug 00000: HOTFIX. Warn removal: 'search' param used as string everywhere but can be absent
    Use of uninitialized value $searchid in concatenation (.) or string at .../cataloguing/additem.pl line 414.

--- cataloguing/additem.pl
+++ cataloguing/additem.pl
@@ -130,7 +130,7 @@ my $biblio = Koha::Biblios->find($biblionumber);
 my $op             = $input->param('op') || q{};
 my $hostitemnumber = $input->param('hostitemnumber');
 my $marcflavour    = C4::Context->preference("marcflavour");
-my $searchid       = $input->param('searchid');
+my $searchid       = $input->param('searchid') // q{};
 
 # fast cataloguing datas
 my $fa_circborrowernumber  = $input->param('circborrowernumber');
