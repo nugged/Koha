d214ced85f1e9092f5228b4a74586eb65f6522d2
Andrii Nugged <nugged@gmail.com>
2025-11-23 07:08:05 +0200

    Bug 20447: (follow-up) Dropdown to select the default holding framework

--- C4/Context.pm
+++ C4/Context.pm
@@ -701,6 +701,7 @@ sub set_userenv_from_session {
         $session->param('emailaddress'), $session->param('shibboleth'),
         $session->param('desk_id'),      $session->param('desk_name'),
         $session->param('register_id'),  $session->param('register_name'),
+        $session->param('default_holding_framework'),
         $session->id,
     );
 }
@@ -727,7 +728,9 @@ sub set_userenv {
         $usernum,      $userid,        $usercnum,   $userfirstname,
         $usersurname,  $userbranch,    $branchname, $userflags,
         $emailaddress, $shibboleth,    $desk_id,    $desk_name,
-        $register_id,  $register_name, $session_id,
+        $register_id,  $register_name,
+        $default_holding_framework,
+        $session_id,
     ) = @_;
 
     my $cell = {
@@ -747,6 +750,7 @@ sub set_userenv {
         "desk_name"     => $desk_name,
         "register_id"   => $register_id,
         "register_name" => $register_name,
+        "default_holding_framework" => $default_holding_framework,
         "session_id"    => $session_id,
     };
     $context->{userenv} = $cell;
